6fdd1578c573b446e5d859e44ae064ebec036d35
programmer15424 <kez61918@gmail.com>
2025-12-01 23:35:10 -0500

    Agregar workflow de GitHub Actions para despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: '/inkamoss',
+  assetPrefix: '/inkamoss',
 }
 
 export default nextConfig
